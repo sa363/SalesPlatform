80e6480ba18e0c42facce17378fe91237b4020e4
Sergey Anisimov <sa@navat.ru>
2016-06-28 17:12:05 +0300

    devel version

--- docker-SalesPlatform/files/createdb.sql
+++ docker-SalesPlatform/files/createdb.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE crm;
 CREATE USER 'crm'@'localhost' IDENTIFIED BY 'PaSsWoRd';
-GRANT ALL PRIVILEGES ON `crm`.* TO 'crm'@'localhost';
+GRANT ALL PRIVILEGES ON crm.* TO 'crm'@'localhost';
 FLUSH PRIVILEGES;
